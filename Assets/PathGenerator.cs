4ccf30f1f7cb443e7dc41583dbd273fd8dbffcab
Matthew Ong <matthew910@gmail.com>
2015-10-17 15:10:53 -0500

    Added algorithm to create path that goes forward and sideways

--- Assets/PathGenerator.cs
+++ Assets/PathGenerator.cs
@@ -2,14 +2,27 @@
 using System.Collections;
 
 public class PathGenerator : MonoBehaviour {
-    public int sizeX, sizeZ;
-
+    public int numBlocks;
     public Block blockPrefab;
+    public int longestPath;
+    public int shortestPath;
 
-    private Block[,] blocks;
+    private Block[] blocks;
+    private int pathCounter;
+    private int pathLength;
+    //Direction: 0 = left, 1 = forward, 2 = right
+    private int direction;
+    private int currentX;
+    private int currentZ;
+
+
+
+
+
+
     // Use this for initialization
     void Start () {
-	
+        direction = 1;
 	}
 	
 	// Update is called once per frame
@@ -19,23 +32,58 @@ public class PathGenerator : MonoBehaviour {
 
     public void Generate()
     {
-        blocks = new Block[sizeX, sizeZ];
-        for (int x = 0; x < sizeX; x++)
+        blocks = new Block[numBlocks];
+        for (int i = 0; i < numBlocks; i++)
         {
-            for (int z = 0; z < sizeZ; z++)
+            //IF REACHED END OF PATH
+            if(pathLength == 0)
             {
-                CreateBlock(x, z);
+                //CHOOSE NEW DIRECTION
+                //IF DIRECTION IS RIGHT OR LEFT, DIRECTION IS ALWAYS FORWARD
+                if(direction == 0 || direction == 2)
+                {
+                    direction = 1;
+                } else
+                {
+                    //IF DIRECTION IS FORWARD, RANDOMLY CHOOSE RIGHT OR LEFT
+                    if(Random.Range(0,2) == 0) {
+                        direction = 0;
+                    } else
+                    {
+                        direction = 2;
+                    }
+                }
+
+
+                //CHOOSE NEW PATH LENGTH
+                pathLength = Random.Range(shortestPath, longestPath + 1);
+
             }
+
+
+            if(direction == 1)
+            {
+                currentX += 2;
+            } else if(direction == 0)
+            {
+                currentZ += 2;
+            } else
+            {
+                currentZ -= 2;
+            }
+
+            CreateBlock(currentX, currentZ, i);
+            pathLength--;
         }
     }
 
-    private void CreateBlock(int x, int z)
+    private void CreateBlock(int x, int z, int i)
     {
         Block newBlock = Instantiate(blockPrefab) as Block;
-        blocks[x, z] = newBlock;
+        blocks[i] = newBlock;
         newBlock.name = "Block " + x + ", " + z;
         newBlock.transform.parent = transform;
-        newBlock.transform.localPosition = new Vector3(x - sizeX * 0.5f + 0.5f, 0f, z - sizeZ * 0.5f + 0.5f);
+        newBlock.transform.localPosition = new Vector3(x, 0, z);
     }
 
 
