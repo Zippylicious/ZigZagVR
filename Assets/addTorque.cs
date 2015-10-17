9eea4fee241139e598f550cc5f6ef5132351da53
Zippylicious <zakthompson97@gmail.com>
2015-10-16 23:20:48 -0500

    mvp commit 1

---
+++ Assets/addTorque.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addTorque : MonoBehaviour {
+	public float torque;
+	public Rigidbody rb;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
+		if(rb.angularVelocity.magnitude < 1.5){
+			rb.AddTorque (transform.right * torque);
+		}
+	}
+}
